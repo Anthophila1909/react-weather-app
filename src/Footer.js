2ffce1893c6bb142973c0837f0bc77d91779d890
Anthophila1909 <elena.w@hotmail.de>
2025-02-13 18:13:26 +0100

    Updated links in footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,22 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      This projekt was build by Elena Würtz <a href="/">GitHub</a> on and is
-      open-sourced on Netlify
+      This projekt was build by Elena Würtz on{" "}
+      <a
+        href="https://github.com/Anthophila1909/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>{" "}
+      and is open-sourced on{" "}
+      <a
+        href="https://elenas-react-weather-app.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
     </div>
   );
 }
