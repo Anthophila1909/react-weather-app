a232b71d7731c065af7dd7c42742b2ffbaa65459
Anthophila1909 <elena.w@hotmail.de>
2025-02-15 09:54:19 +0100

    Basic HTML structure

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function WeatherApp() {
-  return <h1>Hello from WeatherApp</h1>;
+  return (
+    <div className="container weather-app">
+      <div className="row">
+        <div className="col-4">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather-icon"
+          />
+          <h1>-1°C</h1>
+        </div>
+        <div className="col-4">
+          <p>Niederschlag: 0%</p>
+          <p>Luftfeuchte: 66%</p>
+          <p>Wind: 5 km/h</p>
+        </div>
+        <div className="col-4">
+          <h2>Wetter in Ulm</h2>
+          <h3>Samstag, 09:00</h3>
+          <h3>Überwiegend bewölkt</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
